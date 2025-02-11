cd3cc59ac62688710ea757c6f71a660d6f82604d
Laia Hidalgo <laia@hidalgojimenez.es>
2025-02-11 20:08:57 +0100

    renamed folder

---
+++ activities_UNIR_1/act5_angular/blog/src/app/components/lista-articulos/lista-articulos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-articulos',
+  imports: [],
+  templateUrl: './lista-articulos.component.html',
+  styleUrl: './lista-articulos.component.css'
+})
+export class ListaArticulosComponent {
+
+}
